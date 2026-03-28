7b43dd31c39e9beb5fb062b85701113dbd7fa1c0
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2026-03-28 14:46:50 -0400

    clankercode/ Objects

---
+++ app/components/Catagories/wires.jsx
@@ -0,0 +1,31 @@
+// Derived from components/Catagories/phones.jsx: Child component: Wire (Connector, Input/Output, State: Connected | Floating | Shorted)
+// Idea: Carries directional flow between nodes; floating ends trigger console warning
+
+import React from 'react';
+import { Component } from './Components';
+
+const Wire = ({ 
+    id, 
+    name, 
+    initposition,
+    inputNodeId = null, // ID of the node this wire is connected to (if any)
+    outputNodeId = null, // ID of the node this wire is connected to (if any)
+    State = 'not connected' // 'connected', 'Not connected'
+
+
+    }) => {
+    return (
+        <Component 
+            id={id} 
+            name={name}
+            type="wire"
+            initposition={initposition}
+            inputNodeId={inputNodeId}
+            outputNoodeId={outputNoodeId}
+            State={State}
+
+        />
+
+
+    )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
-"use client"; // only if page needs client hooks
 import Simulation from "./components/simulation";
 import Console from "./components/console";
 // import Categories from "./components/categories";
+import Tester from "./components/Catagories/tester";
 
 export default function Home() {
   return (
     <main>
       {/* <Categories /> */}
+      <Tester />
       <Simulation />
       <Console />
     </main>
